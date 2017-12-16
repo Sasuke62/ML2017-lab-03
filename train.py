7089e32f71a22dbcd251b5f0911c46f0cd08ff54
LiuRui <470476516@qq.com>
2017-12-16 15:04:11 +0800

    'train.py'

---
+++ train.py
@@ -0,0 +1,35 @@
+import numpy as np
+import random
+import matplotlib.pylab as plt
+import pickle
+import os
+
+from feature import NPDFeature
+from PIL import Image
+
+def get_image_dir(dir_path):
+    '''返回目录中所有jpg图像的文件名的列表'''
+    return [os.path.join(dir_path, img) for img in os.listdir(dir_path)]
+
+'''
+加载原始图像
+'''
+img_list = get_image_dir('./datasets/original/face')
+num_img_face = len(img_list)
+img_face = []
+
+for i in range(num_img_face):
+    img_face.append(Image.open(img_list[i]))
+
+'''
+将图像转化为灰度
+'''
+img_face[0].show()
+img_face[1].show()
+#img = Image.open('./datasets/original/face/face_000.jpg')
+
+#if __name__ == "__main__":
+    # write your code here
+
+#    pass
+
