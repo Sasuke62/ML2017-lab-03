91e834f08af2da2b89ee9e36d010e63e33761724
LiuRui <470476516@qq.com>
2017-12-16 15:21:15 +0800

    '读图片，resize，转灰度'

--- train.py
+++ train.py
@@ -8,24 +8,27 @@ from feature import NPDFeature
 from PIL import Image
 
 def get_image_dir(dir_path):
-    '''返回目录中所有jpg图像的文件名的列表'''
+    '''
+    返回目录中所有jpg图像的文件名的列表
+    '''
     return [os.path.join(dir_path, img) for img in os.listdir(dir_path)]
 
 '''
-加载原始图像
+加载原始图像并将图像resize成24*24并且转化为灰度图
 '''
 img_list = get_image_dir('./datasets/original/face')
 num_img_face = len(img_list)
 img_face = []
 
 for i in range(num_img_face):
-    img_face.append(Image.open(img_list[i]))
+    img_face.append(Image.open(img_list[i]).convert('L'))
+    img_face[i].thumbnail((24, 24))
 
-'''
-将图像转化为灰度
-'''
+
 img_face[0].show()
 img_face[1].show()
+
+
 #img = Image.open('./datasets/original/face/face_000.jpg')
 
 #if __name__ == "__main__":
